ca8f82fd46475316aa42e5580f2d1e90e7686d9f
Max Jacobson <maxwell.jacobson@gmail.com>
2012-12-01 07:44:14 -0500

    post 3

---
+++ config.rb
@@ -0,0 +1,7 @@
+def get_title
+  return "max jacobson&rsquo;s devblog"
+end
+
+def get_subtitle
+  return "home"
+end
--- web.rb
+++ web.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 require 'kramdown'
+require_relative 'config'
 
 get '/' do
-  @title = "devblog - max jacobson"
+  @title = get_title
+  @subtitle = get_subtitle
   the_html = String.new
   posts = Dir.entries("posts")
   posts.each do |filename|
@@ -17,22 +19,25 @@ get '/' do
 end
 
 get '/*/' do
-  @title = "devblog - max jacobson"
   the_html = String.new
   naked_filename = params[:splat][0].to_s
   the_text = File.read("posts/" + naked_filename + ".md")
   the_html << Kramdown::Document.new(the_text).to_html
   the_html << "<a href=\"/#{naked_filename}.md\">Markdown source</a>\n<hr />\n"
+  @title = get_title
+  @subtitle = naked_filename
   erb the_html
 end
 
 get '/*.md' do
-  @title = "devblog - max jacobson"
   filename = "posts/" + params[:splat][0].to_s + ".md"
+  naked_filename = params[:splat][0].to_s
   the_text = File.read(filename)
   the_text.gsub!(/</, '&lt;')
   the_text.gsub!(/>/,'&gt;')
-  erb "<h2>Markdown source of #{params[:splat][0].to_s + ".md"}</h2>\n\n<pre>\n" + the_text + "\n</pre>"
+  @title = get_title
+  @subtitle = "markdown source of " + naked_filename
+  erb "<h2>Markdown source of <a href=\"#{naked_filename}/\">#{params[:splat][0].to_s + ".md"}</a></h2>\n\n<pre>\n" + the_text + "\n</pre>"
 end
 
 get '/css/style.css' do
